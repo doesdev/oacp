e79f17786bbb8cb152c19ee26f492c46898e6b6e
Andrew Carpenter <andrew@audioinhd.com>
2015-10-28 19:52:05 -0400

    Set namespace from init

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function oacp (namespace) {
   return self
 }
 // oacp.prototype.Server = require('./lib/server')
-oacp.prototype.registerModel = (model) => require('./lib/model')(model)
+oacp.prototype.registerModel = (model) => require('./lib/model')(this.ns, model)
 // oacp.prototype.Controller = require('./lib/controller')
 
 // Exports
