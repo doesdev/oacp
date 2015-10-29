df3ce96062a65bfc50118f5e6775eb25f9d678f0
Andrew Carpenter <andrew@audioinhd.com>
2015-10-29 15:12:55 -0400

    Make paths relative to app root

--- config/app.js
+++ config/app.js
@@ -1,26 +1,33 @@
 // Setup
 const path = require('path')
+const appRoot = require('app-root-path').toString()
 const Logger = require('./../lib/logger')
 const fs = require('fs')
-const info = require('./../package.json')
+const info = require(path.join(appRoot, 'package.json'))
 var config = {app: {}}
 config.app.name = info.name
 config.app.ns = config.app.namespace = (info.namespace || info.name)
   .replace(/\.?([A-Z]+)/g, (x, y) => (' ' + y)).trim()
   .replace(/\s+/g, '_').toLowerCase()
 config.app.version = info.version
-const pubKey = fs.readFileSync(path.join(__dirname, config.app.ns + '.pub'))
+const privKeyPath = path.join(appRoot, 'config', config.app.ns + '.priv')
+try {
+  config.privKey = fs.readFileSync(privKeyPath)
+} catch (e) {}
+const pubKeyPath = path.join(appRoot, 'config', config.app.ns + '.pub')
+try {
+  config.pubKey = fs.readFileSync(pubKeyPath)
+} catch (e) {}
 
 var loggerOpts = {
   appName: info.name,
   toConsole: true,
   toFile: true,
   loglevel: 'debug',
-  logBase: path.join(__dirname, '../', 'logs')
+  logBase: path.join(appRoot, 'logs')
 }
 createDir(loggerOpts.logBase)
 config.logger = new Logger(loggerOpts)
-config.pubKey = pubKey
 
 module.exports = config
 
