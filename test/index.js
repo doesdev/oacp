079a6948b9f76cca8598911d00712be6d4f826fe
Andrew Carpenter <andrew@audioinhd.com>
2015-10-29 14:18:40 -0400

    Add some model tests, corresponding bugfixes

--- test/index.js
+++ test/index.js
@@ -1,9 +1,12 @@
+// Setup
 const namespace = 'testapp'
 const Oacp = require('./../index')
 var app = new Oacp(namespace)
 const assert = require('assert')
+// Mock model
 function User () {}
 
+// Tests
 describe('Oacp', function () {
   describe('app', () =>
     it('should be instance of oacp', () =>
@@ -18,10 +21,20 @@ describe('Oacp', function () {
   describe('app.registerModel(User)', function () {
     app.registerModel(User)
     it('should extend Record on User', () =>
-      assert(User.new instanceof Function)
+      assert(User.find instanceof Function)
     )
-    it('should cause User to inherit from Record', () =>
-      assert((User.new()).save instanceof Function)
+  })
+  describe('User.new()', function () {
+    app.registerModel(User)
+    var user = User.new()
+    it('should return instance of User', () =>
+      assert.equal(user.constructor.name, 'User')
+    )
+    it('should inherit from Record', () =>
+      assert(user.save instanceof Function)
+    )
+    it('should inherit from EventEmitter', () =>
+      assert(user.emit instanceof Function)
     )
   })
 })
